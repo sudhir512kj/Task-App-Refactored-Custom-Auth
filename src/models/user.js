8c690c4c7d81d595b4e3c7476de2e4e058faf6b8
JamieCorkhill <jamie.corkhill@Live.com>
2019-07-28 16:29:34 -0500

    Added user model

---
+++ src/models/user.js
@@ -0,0 +1,71 @@
+/*
+ * File: (src/models/user.js)
+ *
+ * Description: This file contains the Schema, Model Definition, and custom middleware for the User Model. This represents the schema of data
+ * relating to users as they exist in the database.
+ * 
+ * Created by Jamie Corkhill 07/28/2019 at 04:26 PM (Local), 07/28/2019 at 09:26 PM (Zulu)
+ */
+
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// Define the schema.
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password can not contain password');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Don\'t be stupid.');
+            }
+        }
+    },
+    avatarPaths: {
+        type: String,
+        original: { type: String, default: 'no-profile' },
+        small: { type: String, default: 'no-profile' },
+        large: { type: String, default: 'no-profile' }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    // Keep track of last updates and the Unix Timestamp when the profile was created.
+    timestamps: true
+});
+
+// Create the model.
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
