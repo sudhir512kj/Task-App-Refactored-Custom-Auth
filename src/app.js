9758919886c0e98333310cf2147c5d326de1e8e4
JamieCorkhill <jamie.corkhill@Live.com>
2019-07-28 16:23:36 -0500

    Add Express App and server boilerplate

---
+++ src/app.js
@@ -0,0 +1,45 @@
+/*
+ * File: app.js (src/app.js)
+ *
+ * Description: This file handles the main operations of the server. It sets up Express and its middleware and handles all data. Additionally, it defines
+ * CRUD Routes and configures Dependency Injection into each route.
+ * 
+ * Created by Jamie Corkhill on 07/28/2019 at 04:18 PM (Local), 09:18 PM (Zulu)
+ */
+
+const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// Dependency Injection
+const { scopePerRequest } = require('awilix-express');
+
+// Middleware
+// const handleErrors = require('./api/middleware/handle-errors');
+
+// Delegate errors in async endpoints to be handled by middleware.
+require('express-async-errors');
+
+// Routes - User
+// const userRoutes = require('./api/routes/user');
+
+// Application Factory to ease Dependency Injection in tests.
+module.exports = container => {
+    // Create the Express Application.
+    const app = express();
+
+    // Express Middleware.
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(mongoSanitize());
+
+    // Awilix Dependency Injection.
+    app.use(scopePerRequest(container));
+
+    // Routes - Users
+    // app.use('/api/v1/users', userRoutes);
+
+    // This needs to come last to handle errors.
+    // app.use(handleErrors);
+
+    return app;
+};
---
+++ src/server.js
@@ -0,0 +1,20 @@
+/*
+ * File: server.js (src/server.js - entry)
+ *
+ * Description: This file imports the Express application and binds it to a port. It is done this way to permit Unit/Integration Testing via the Supertest
+ * library, which requires an Express Application and not an operating web server. We also start the database connection here.
+ *
+ * Created by Jamie Corkhill on 07/28/2019 at 04:21 PM (Local), 09:21 PM (Zulu)
+ */
+
+// Import the database connection factory and fire it.
+// require('./db/mongoose')();
+
+// Import the Express Application.
+const appFactory = require('./app');
+
+// Awilix Dependency Injection Container
+// const container = require('./container/containerFactory')();
+
+// Bind the server to the development port or whatever is available in production.
+// appFactory(container).listen(process.env.PORT, () => console.log(`Server is up on port ${process.env.PORT}`));
